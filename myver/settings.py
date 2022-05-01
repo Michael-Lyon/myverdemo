d415accb5ce3a78ef2b66f227b2f922264261479
Michael-Lyon <mickiasomg@gmail.com>
2022-05-01 13:22:59 +0100

    static files

--- myver/settings.py
+++ myver/settings.py
@@ -112,7 +112,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- myver/urls.py
+++ myver/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from myver.settings import STATIC_ROOT, STATIC_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('apiv1/', include('api.urls'))
-]
+] + static(STATIC_URL, document_root=STATIC_ROOT)
